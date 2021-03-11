d7f736435aaa64f75a1f244b2b98e3b351420e11
Gabriel Lima Luz <gabriel.lima.luz@hotmail.com>
2021-03-11 17:11:16 -0300

    put implementado

--- api/loja/urls.py
+++ api/loja/urls.py
@@ -3,5 +3,6 @@ from  loja import views
 
 urlpatterns = [
     path("",views.get_livros, name="get_livros"),
-    path("create/", views.add_livro, name="add_livro")
+    path("create/", views.add_livro, name="add_livro"),
+    path("update/<int:id_livro>", views.update_livro)
 ]
--- api/loja/views.py
+++ api/loja/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from .serializers import LivroSerializer
+from django.core import serializers
 from .models import Livro, Autor
 import json
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,8 +12,7 @@ import datetime
 @api_view(["GET"])
 def get_livros(request):
     livros = Livro.objects.all()
-    serializer = LivroSerializer(livros, many=True)
-    return JsonResponse({'livros': serializer.data},safe=False, status=status.HTTP_200_OK)
+    return JsonResponse({'livros': serializers.serialize('json',livros)},safe=False, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
@@ -23,14 +22,25 @@ def add_livro(request):
         autor = Autor.objects.get(nome=payload["autor"])
     except ObjectDoesNotExist:
         autor = Autor.objects.create(nome=payload["autor"])
-
+    
     livro = Livro.objects.create(
         nome = payload["nome"],
         autor = autor,
         quantidade_de_paginas = payload["quantidade_de_paginas"],
         preco= payload["preco"]
     )
-    serializer = LivroSerializer(livro)
-    return JsonResponse({'livros':serializer.data},safe=False, status = status.HTTP_201_CREATED)
+    livro = Livro.objects.filter(nome=payload["nome"])
+    return JsonResponse({'livros':serializers.serialize("json",livro)},safe=False, status = status.HTTP_201_CREATED)
 
-    
+@api_view(["PUT"])
+def update_livro(request, id_livro):
+    payload = json.loads(request.body)
+    try:
+        livro = Livro.objects.filter(id=id_livro)
+        livro.update(**payload)
+        livro = Livro.objects.filter(id=id_livro)
+        livro = serializers.serialize('json', livro)
+        return JsonResponse({'book': livro}, status=status.HTTP_200_OK)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+        
